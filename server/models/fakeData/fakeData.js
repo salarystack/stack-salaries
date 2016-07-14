9205412e91e0f39906ff1b18235b1eb13ffabb7d
Minyens <xminhhainguyen@gmail.com>
2016-07-14 18:06:23 -0400

    Add fake data that will be used to test features. Must add to database.

---
+++ server/models/fakeData/fakeData.js
@@ -0,0 +1,42 @@
+var fakeData = [
+  { state: 'CA',
+    city: 'San Jose',
+    salary: 150000
+    stack: ['javascript']
+  },
+  { state: 'CA',
+    city: 'San Jose',
+    salary: 130000
+    stack: ['javascript', 'angular']
+  },
+  { state: 'CA',
+    city: 'San Diego',
+    salary: 110000
+    stack: ['javascript', 'react']
+  },
+  { state: 'CA',
+    city: 'San Jose',
+    salary: 135000
+    stack: ['javascript', 'angular']
+  },
+  { state: 'NY',
+    city: 'New York',
+    salary: 150000
+    stack: ['javascript', 'angular']
+  },
+  { state: 'NY',
+    city: 'New York',
+    salary: 100000
+    stack: ['javascript', 'backbone']
+  },
+  { state: 'NY',
+    city: 'New York',
+    salary: 106000
+    stack: ['javascript', 'react']
+  },
+  { state: 'NY',
+    city: 'New York',
+    salary: 96000
+    stack: ['javascript', 'angular']
+  }
+]
