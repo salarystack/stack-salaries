799d11ccd818561642ca43b4a1e658eaf0053e7d
AJ Zawawi <aminojzawawi@gmail.com>
2016-07-14 10:08:38 -0400

    Update config files and initial setup.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  entry: [
+    './client/src/components/app.jsx',
+      './client/src/components/cloud.jsx',
+      './client/src/components/footer.jsx',
+      './client/src/components/login.jsx',
+      './client/src/components/logo.jsx',
+      './client/src/components/main.jsx',
+      './client/src/components/results.jsx',
+      './client/src/components/search.jsx',
+      './client/src/index.js',
+  ],
+  output: {
+    path: './client/compiled/src',
+    filename: 'bundle.js'
+   },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    hot: true
+  }
+
+};
