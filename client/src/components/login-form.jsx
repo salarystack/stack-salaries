e0f69dca8c917c405e77343b809c0e4f7aeb6b48
Nahee Kwak <naheekw@gmail.com>
2016-07-18 15:19:35 -0400

    Add loginForm

--- client/src/components/login-form.jsx
+++ client/src/components/login-form.jsx
@@ -8,12 +8,30 @@ const LoginForm = React.createClass({
       authToken: ''
     };
   }
-  loginToServer (user, password) {
+
+  changeUser(e) {
+    this.setState({
+      user: e.target.value
+    })
+  }
+
+  changePassword(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+
+  loginToServer (e) {
+    e.preventDefault();
+
+    var data = {user: this.state.user, password: this.state.password};
+
     $.ajax({
       url:'http://localhost:3000/signin',
       type:'POST',
       contentType:"application/json",
-      data: {name: user, password: password}
+      data: JSON.stringify(data);
       success: function(data) {
         this.setState({
           authToken: data;
@@ -27,13 +45,15 @@ const LoginForm = React.createClass({
 
   render: function() {
     return (
-      <form role="form">
-      <div className="form-group">
-        <input type="text" valueLink={this.loginToServer('user')} placeholder="Username" />
-        <input type="password" valueLink={this.loginToServer('password')} placeholder="Password" />
-      </div>
-      <button type="submit" onClick={this.login.bind(this)}>Submit</button>
-      </form>
+      <div>
+        <form role="form">
+        <div className="form-group">
+          <input type="text" changeUser={this.changeUser.bind(this)} placeholder="Username" />
+          <input type="password" changePassword={this.changePassword.bind(this)} placeholder="Password" />
+        </div>
+        <button type="submit" onClick={this.loginToServer.bind(this)}>Submit</button>
+        </form>
+    </div>
     );
   }
 });
