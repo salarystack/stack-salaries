8553dd24b2774a37f73bbf6cef87752bec2159fe
AJ Zawawi <aminojzawawi@gmail.com>
2016-07-18 10:30:48 -0400

    Add bootstrap input groups to search form.

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -4,9 +4,15 @@ const Search = React.createClass({
   render: function() {
     return (
       <div>
-        <input type='text' id='stacks'/>
-        <input type='text' id='location'/>
-        <button>Search</button>
+        <form className="flexcontainer">
+        <div className="input-group">
+          <input type="text" className="form-control" placeholder="Add your tech stack separated by commas"/>
+          <input type="text" className="city form-control" placeholder="New York, NY"/>
+          <button className="btn btn-primary"><span className="glyphicon glyphicon-search"></span>  Search</button>
+        </div>
+
+
+        </form>
       </div>
     );
   }
