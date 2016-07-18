374943960dee849cb44f5934c6c64fe0642bc0f7
AJ Zawawi <aminojzawawi@gmail.com>
2016-07-18 10:31:13 -0400

    Adjust style on the main component.

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -4,8 +4,8 @@ const Main = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>Stack Salaries</h1>
-        <h2>An app for software engineers to post and view anonymous salary info based on their stack and location.</h2>
+        <h1 className="feature">Stack Salaries</h1>
+        <p>An app for software engineers to post and view anonymous salary info based on their stack and location.</p>
       </div>
     );
   }
