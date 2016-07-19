6db6f50693465d1a2b6694beb27fde2c363929f3
Nahee Kwak <naheekw@gmail.com>
2016-07-19 13:26:12 -0400

    Test

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -8,7 +8,7 @@ const Search = React.createClass({
         <div className="input-group">
           <input type="text" className="form-control" placeholder="Add your tech stack separated by commas"/>
           <input type="text" className="city form-control" placeholder="New York, NY"/>
-          <button className="btn btn-primary"><span className="glyphicon glyphicon-search"></span>  Search</button>
+          <button className="btn btn-primary"><span className="glyphicon glyphicon-search"></span>Search</button>
         </div>
 
 
