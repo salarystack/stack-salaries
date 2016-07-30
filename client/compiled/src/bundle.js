9a45c62fbe290c6a427b8f711a767ec749e04636
Sujin Lee <pasdesujin@gmail.com>
2016-07-30 15:41:13 -0400

    Add image, description, and sending email feature with new style

--- client/compiled/src/bundle.js
+++ client/compiled/src/bundle.js
@@ -49449,6 +49449,23 @@
 	  }
 
 	  _createClass(Premium, [{
+	    key: 'toggle',
+	    value: function toggle() {
+	      (0, _jquery2.default)(".resumeBtn").unbind('click').click(function () {
+	        (0, _jquery2.default)(".recruiterBox").toggle(function () {
+	          console.log('toggled');
+	        });
+	      });
+	    }
+	  }, {
+	    key: 'contactRecruiter',
+	    value: function contactRecruiter() {
+	      var email = "RecruiterSujin@gmail.com";
+	      var subj = "Inquiry regarding ";
+	      var message = "Dear Sujin,";
+	      window.open("mailto:" + email + "?subject=" + subj + "&body=" + message, "_self");
+	    }
+	  }, {
 	    key: 'componentWillMount',
 	    value: function componentWillMount() {}
 	  }, {
@@ -49458,18 +49475,40 @@
 	        'div',
 	        { className: 'row' },
 	        _react2.default.createElement(
-	          'button',
+	          'h1',
 	          null,
+	          ' Premium Content '
+	        ),
+	        _react2.default.createElement(
+	          'button',
+	          { className: 'resumeBtn btn btn-default', onClick: this.toggle.bind(this) },
 	          'Resume + Recruiter'
 	        ),
 	        _react2.default.createElement(
-	          'form',
-	          null,
-	          _react2.default.createElement(_reactFileInput2.default, { name: 'myImage',
-	            accept: '.png,.gif',
-	            placeholder: 'My Image',
-	            className: 'inputClass',
-	            onChange: this.handleChange })
+	          'div',
+	          { className: 'recruiterBox panel panel-default' },
+	          _react2.default.createElement(
+	            'div',
+	            { className: 'panel-body' },
+	            _react2.default.createElement(_reactFileInput2.default, { name: 'myImage',
+	              accept: '.docx,.jpg',
+	              placeholder: 'Upload Resume',
+	              className: 'inputClass',
+	              onChange: this.handleChange }),
+	            _react2.default.createElement('br', null),
+	            _react2.default.createElement('img', { className: 'media-object img-circle', src: 'https://s.gravatar.com/avatar/cc2623f773bcdd3b6b8765c33f1ec5a1?s=200' }),
+	            _react2.default.createElement('br', null),
+	            _react2.default.createElement(
+	              'p',
+	              null,
+	              ' Hi, my name is Sujin and I have over 10 years of experience as a technical recruiter.  I look forward to working with you! '
+	            ),
+	            _react2.default.createElement(
+	              'button',
+	              { className: 'btn btn-default', onClick: this.contactRecruiter.bind(this) },
+	              ' Contact Recruiter '
+	            )
+	          )
 	        )
 	      );
 	    }
